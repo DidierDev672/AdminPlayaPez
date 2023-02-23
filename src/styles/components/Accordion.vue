4a4f9c2676820ef4abce349703759a89bd95f760
DidierDev672 <ariasdev672@gmail.com>
2023-02-23 12:13:02 +0100

    Ordering Accordion

---
+++ src/styles/components/Accordion.vue
@@ -0,0 +1,88 @@
+<script>
+import ItemAccordion from "./ItemAccordion.vue";
+export default {
+  name: "accordion-view",
+  data() {
+    return {
+      data: [
+        {
+          id: 9,
+          code_buy: "acb1b854-c83f-4ba7-8993-152e06db4dfb",
+          category: "Medidor",
+          name_product: "Detector de concentración de oxígeno O2 Portatil",
+          price: 458565,
+          quantity: 1,
+          description:
+            "El detector de oxígeno portátil es un tipo de instrumento de seguridad esencial que puede detectar continuamente la concentración de oxígeno en el entorno de trabajo.",
+        },
+        {
+          id: 4,
+          code_buy: "acb1b854-c83f-4ba7-8993-152e06db4dfb",
+          category: "Sensor",
+          name_product: "Sensor de temperatura",
+          price: 20900,
+          quantity: 1,
+          description:
+            "Nivel de líquido agua sensor detectar electrodos industrial (ENTREGA INMEDIATA)",
+        },
+        {
+          id: 10,
+          code_buy: "98d28ec4-9eb3-4594-96df-05645d4eb441",
+          category: "Alimentador",
+          name_product:
+            "Dispensador Automatico de comida para peces Resun Af 2009 D",
+          price: 115000,
+          quantity: 1,
+          description:
+            "Dispensador automatico para peces Marca Resun Referencia AF 2009 A",
+        },
+        {
+          id: 11,
+          code_buy: "98d28ec4-9eb3-4594-96df-05645d4eb441",
+          category: "Alimentador",
+          name_product: "Filtro extremo de cascada Resun 570 L/h Acuarios",
+          price: 64500,
+          quantity: 1,
+          description:
+            "Filtro externo de cascada Resun HF 2002, Consumo 6 W Capacidad de bombeo 570 L/H",
+        },
+      ],
+      queryData: [],
+    };
+  },
+
+  props: {
+    code_buy: String,
+  },
+
+  components: {
+    ItemAccordion,
+  },
+
+  created() {
+    let query = Object.values(this.data).filter(
+      (item) => item.code_buy === this.code_buy
+    );
+    this.queryData.push(...query);
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="content-acordion">
+      <h3>Resumen del pedido</h3>
+      <item-accordion
+        v-for="(list, index) in queryData"
+        :key="index"
+        :id="list.id"
+        :category="list.category"
+        :description="list.description"
+        :name_product="list.name_product"
+        :price="list.price"
+        :quantity="list.quantity"
+        :isOpen="false"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/styles/components/ItemAccordion.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+  name: "item-accordion",
+  data() {
+    return {
+      Open: this.isOpen,
+    };
+  },
+  props: {
+    id: Number,
+    category: String,
+    description: String,
+    name_product: String,
+    price: Number,
+    quantity: Number,
+    isOpen: Boolean,
+  },
+
+  methods: {
+    visible() {
+      if(this.Open === false){
+        this.Open = true;
+      }
+
+      // if(this.Open === true){
+      //   this.Open = false;
+      // }
+    },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <div class="accordion-content">
+      <div class="content-summary">
+        <div>
+          <h4>{{ name_product }}</h4>
+        </div>
+        <div>
+          <button type="button" class="btn-arrow" @click="visible">
+            <i class="fa-solid fa-arrow-down"></i>
+          </button>
+        </div>
+      </div>
+      <ul :class="{ visible: Open }">
+        <li>{{ name_product }}</li>
+        <li>{{ category }}</li>
+        <li>{{ price }}</li>
+        <li>{{ quantity }}</li>
+        <li>{{ description }}</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.accordion-content {
+  border-radius: 0.5rem;
+  border: 2px solid #daeaf1;
+  box-shadow: 6px 6px 6px #deedf0;
+  padding: 0.5rem;
+  place-content: center;
+}
+.content-summary {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-rows: minmax(50px, auto);
+  gap: 20px;
+}
+
+.btn-arrow {
+  border: none;
+  cursor: pointer;
+  position: relative;
+  right: -360px;
+  margin-top: 10px;
+}
+
+ul {
+  display: none;
+}
+
+.visible {
+  display: block;
+}
+</style>
