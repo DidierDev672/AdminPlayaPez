de654482948bb9a0da3511e630f7e077cfb7af9f
DidierDev672 <ariasdev672@gmail.com>
2023-03-09 00:02:38 -0500

    New ordering module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, createLogger } from "vuex";
 import product from "./modules/product";
+import order from "./modules/order";
 
 const debug = process.env.NODE_ENV !== "production";
 
 export default createStore({
   modules: {
     product,
+    order,
   },
   strict: debug,
   plugins: debug ? [createLogger()] : [],
