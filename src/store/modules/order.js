cfaf1936058d45c03e4ffedd59c28d3ab96cbf94
DidierDev672 <ariasdev672@gmail.com>
2023-03-10 12:13:25 -0500

    Order script

--- src/store/modules/order.js
+++ src/store/modules/order.js
@@ -3,6 +3,7 @@ import API from "../../api";
 
 const state = () => ({
   orders: [],
+  detail: [],
   fetchingData: true,
   error: null,
 });
@@ -10,6 +11,12 @@ const state = () => ({
 const getters = {
   getListbyOrders: (state) => (code_buy) => {
     return Object.values(state.orders).filter(
+      (list) => list.code_buy === code_buy
+    );
+  },
+
+  getListbyDetail: (state) => (code_buy) => {
+    return Object.values(state.detail).filter(
       (list) => list.code_buy === code_buy
     );
   },
@@ -22,6 +29,20 @@ const actions = {
     API.getOrder()
       .then((result) => commit(types.FETCH_ORDER_SUCESS, { orders: result }))
       .catch((error) => commit(types.FETCH_ORDER_FAILURE, { error }));
+  },
+
+  fetchDetail({ commit }) {
+    commit(types.FETCH_DETAIL_REQUEST);
+
+    API.getDetailOrder()
+      .then((result) => commit(types.FETCH_DETAIL_SUCESS, { detail: result }))
+      .catch((error) => commit(types.FETCH_DETAIL_FAILURE, { error }));
+  },
+
+  updateStateOrder({ commit, id, state_order, note }) {
+    commit(types.FETCH_ORDER_REQUEST);
+
+    API.updateOrders({ id, state_order, note });
   },
 };
 
@@ -38,6 +59,22 @@ const mutations = {
   },
 
   [types.FETCH_ORDER_FAILURE](state, { error }) {
+    state.fetchingData = false;
+    state.error = error;
+  },
+
+  [types.FETCH_DETAIL_REQUEST](state) {
+    state.fetchingData = true;
+    state.error = null;
+  },
+
+  [types.FETCH_DETAIL_SUCESS](state, { detail }) {
+    state.fetchDetail = false;
+    state.error = null;
+    state.detail = { ...detail };
+  },
+
+  [types.FETCH_DETAIL_FAILURE](state, { error }) {
     state.fetchingData = false;
     state.error = error;
   },
